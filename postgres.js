476b3da5d97b1c809d38df50600d9d9c8402a833
Shubham jaiswal <68012742+Hasher2@users.noreply.github.com>
2024-01-18 11:19:13 +0530

    node db name1

--- postgres.js
+++ postgres.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 
  const s = new Sequelize('node_zf2y','node_zf2y_user', 'kMYZmTCtdj4jywlUdnNwe9k0MHdKQyxi', {
-    host: 'dpg-cmkbe2ta73kc73bkftn0-a'
+    host: 'dpg-cmkbe2ta73kc73bkftn0-a',
+    dialect: 'postgres'
   });
 
 export default s;
